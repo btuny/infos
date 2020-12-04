017ed42a974bd53c734d1ed92a3bca8fe745fcf4
Er3nity <75038571+Er3nity@users.noreply.github.com>
2020-12-04 15:22:29 +0300

    Create s.java

---
+++ s.java
@@ -0,0 +1,32 @@
+/**
+ * SaaS.
+ * 
+ * @author (Er3nity) 
+ * @version (v1.0)
+ */
+public class S
+{
+    private int x;
+
+    /**
+     * objects of class s
+     */
+    public S()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * public Image getImage(URL url,
+             String name)
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // xyzzers
+        return x + y;
+    }
+}
